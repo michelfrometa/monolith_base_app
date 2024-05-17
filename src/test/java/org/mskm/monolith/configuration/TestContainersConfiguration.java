e0e77878aa9ab073684042a0681faeab97c8d79e
Michel Lazaro Frometa Burey <michel@linkapital.com.br>
2024-05-17 09:59:06 -0300

    Adding initial Kafka Testcontainers

--- src/test/java/org/mskm/monolith/configuration/TestContainersConfiguration.java
+++ src/test/java/org/mskm/monolith/configuration/TestContainersConfiguration.java
@@ -3,7 +3,9 @@ package org.mskm.monolith.configuration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainersConfiguration {
@@ -15,4 +17,13 @@ public class TestContainersConfiguration {
                 .withReuse(true)
                 .withExposedPorts(27017);
     }
+
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"))
+                .withReuse(true)
+                .withKraft();
+    }
+
 }
